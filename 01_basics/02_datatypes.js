80d9096dbc0b7af6e86bdd288f20142179b411ca
SinghMonika1324 <m9198950@gmail.com>
2024-07-13 21:54:17 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" ; // treat all JS code as newer version
+
+//alert(3 +3) // we are using nodejs , not browser 
+
+console.log(  3+3);  // code readability should be high
+
+console.log("Monika");
+
+let name  ="Monika"
+let age =20
+let isLoggedIn = false
+
+//primitive data types
+//number => 2 to power 53
+//bigint
+// string=> ""
+//boolean => true / false
+//null => standalone value 
+//undefined =>
+//symbol=>unique
+
+
+//objects
+
+console.log(typeof "monika");
+console.log(typeof age);
+console.log(typeof undefined) // type-> undefined
+console.log(typeof null); // type->object
