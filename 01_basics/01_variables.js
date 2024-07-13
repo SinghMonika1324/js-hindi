f846da2b9fcea6cbfbe14022a32a53ad431a11a9
SinghMonika1324 <m9198950@gmail.com>
2024-07-13 21:42:10 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 75747
+let  accountEmail = "monikasingh@gmail.com"
+var accountPassword ="67578"
+
+accountCity ="Mumbai"
+let accountState ;
+
+accountEmail = "monikasinggfyhh@gmail.com"
+var accountPassword ="6254458"
+
+accountCity ="uttarpradesh"
+
+console.log(accountId);
+/* prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId , accountEmail, accountPassword , accountCity , accountPassword])
+
