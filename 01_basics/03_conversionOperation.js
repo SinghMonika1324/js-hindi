1976bfde80827bd32c2efe09c44a910b8242d701
SinghMonika1324 <m9198950@gmail.com>
2024-07-14 09:54:14 +0000

    conversion and operation done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,70 @@
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "22" => 22
+//"33abc" => NaN
+// true=> 1 ; false=> 0
+//null => 0
+//undefined => NaN(Not an Number)
+
+let isLoggedIn =1 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true ; 0=> false
+//"" =>false ; "monika" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// ************************************Operations*********************************************************
+
+let  value= 3
+let negValue = -value
+console.log(negValue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);  //2 to the power 3
+console.log(2/3);
+console.log(2%3);
+*/
+
+let str1 = "hello"
+let str2 = " monika"
+
+let str3 = str1 +str2
+//console.log(str3);
+
+//console.log("1" +2); //12
+//console.log(1 +"2");//12
+//console.log("1" + 2 +2); //122
+//console.log(1 + 2 +"2"); //32
+
+console.log(true);  //true
+console.log(+true);  //1
+console.log(+"");  //0
+
+let num1 , num2, num3
+ 
+num1 = num2 = num3 = 2 + 2 //don't do
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
