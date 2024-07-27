68600134c8a1af40ab72b214033c43f2cec2c9f8
SinghMonika1324 <m9198950@gmail.com>
2024-07-27 17:34:16 +0000

    contol flow done

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -26,3 +26,24 @@ const emptyObj ={}
 if(Object.keys(emptyObj).length===0){
 console.log("Oject is Empty");
 }
+
+//Nullish Coalescing Operator (??): null undefined
+
+let val1;
+//val1 = 5?? 10 -> 5
+//val1 = null ??10 -> 10
+
+//var1 = undefined ?? 15 -> 15
+
+//val1 = null ??10?? 20 -> 10
+
+
+
+//04    truthconsole.log(var1);
+
+//TERNARY OPERATOR
+
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >=80 ? console.log("less than 80") : console.log("more than 80");
